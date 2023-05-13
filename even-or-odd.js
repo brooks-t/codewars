3a42e28f839bd90a89a879600c4878352bb00d43
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-05-13 12:45:43 -0400

    added boiler plate and solved first problem

---
+++ even-or-odd.js
@@ -0,0 +1,9 @@
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd";
+    }
+}
+
+const evenOdd = (number) => (number % 2 === 0 ? "Even" : "Odd");
