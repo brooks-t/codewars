da57cfdab98526c3712b400fa910fca9780b2ca4
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-05-20 13:40:19 -0400

    solved quarter of year challenge

---
+++ quarter-of-year.js
@@ -0,0 +1,10 @@
+// Quarter of the year.
+// Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/javascript
+
+const quarterOf = (month) => {
+    if (month <= 3) return 1;
+    else if (month <= 6) return 2;
+    else if (month <= 9) return 3;
+    else return 4;
+}
