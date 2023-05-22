54e0d2f65d12f37277d95cc805c7c93001df4592
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-05-22 18:23:07 -0400

    solved get hydrated challenge

---
+++ keep-hydrated.js
@@ -0,0 +1,9 @@
+// Keep Hydrated: https://www.codewars.com/kata/582cb0224e56e068d800003c
+
+let time = 3.5;
+
+function litres(time) {
+    return Math.floor(time * 0.5);
+}
+
+console.log(litres(time));
