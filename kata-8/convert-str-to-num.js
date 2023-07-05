98d2de73a34ba71f86974c00c94ae61722878cb7
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-07-05 12:17:59 -0400

    solved string to number

---
+++ kata-8/convert-str-to-num.js
@@ -0,0 +1,6 @@
+// (8 kyu) CONVERT A STRING TO A NUMBER
+// https://www.codewars.com/kata/544675c6f971f7399a000e79/train/javascript
+
+const stringToNumber = (str) => parseInt(str);
+
+console.log("123456");
