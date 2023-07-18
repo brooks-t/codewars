af6f50f9a63ca7bb1c5ed2b2091acfd4b7ddf714
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-07-18 11:23:33 -0400

    solved tube strike options challenge

---
+++ kata-7/tube-strike-options-calculator.js
@@ -0,0 +1,23 @@
+// (7 kyu) Tube Strike Options Calculator: https://www.codewars.com/kata/568ade64cfd7a55d9300003e/train/javascript
+let walk = 5;
+let bus = 8;
+
+const calculator = (distance, busDrive, busWalk) => {
+	const walkTime = distance / walk;
+	const busTime = busDrive / bus + busWalk / walk;
+	if (walkTime > 2) {
+		return "Bus";
+	} else if (walkTime < 0.17) {
+		return "Walk";
+	} else if (walkTime === busTime) {
+		return "Walk";
+	} else if (busTime < walkTime) {
+		return "Bus";
+	} else if (walkTime < busTime) {
+		return "Walk";
+	} else {
+		return "Something went wrong!";
+	}
+};
+
+console.log(calculator(4, 5, 1));
